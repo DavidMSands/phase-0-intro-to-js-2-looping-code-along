e36de66990eefa00bbe6ff68f564ac9077eb4392
David <Davidmaxsands@gmail.com>
2022-01-01 15:41:21 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(name, event) {
+  const newArray = [];
+  for(let i = 0; i < name.length; i++) {
+    const note = `Thank you, ${name[i]}, for the wonderful surprise gift!`;
+    newArray.push(note);
+  }
+  return newArray
+}
+
+function countDown(number) {
+    while(number > -1)
+    console.log(number--)
+}
